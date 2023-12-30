701b3ad022c293e63523efb9035f44503f8fc79f
Detusndx <154344254+Detusndx@users.noreply.github.com>
2023-12-29 20:19:45 -0500

    Update LoggerWrapper.swift

--- AdGuardSDK/AdGuardSDK/SharedAdGuardSDK/Logger/LoggerWrapper.swift
+++ AdGuardSDK/AdGuardSDK/SharedAdGuardSDK/Logger/LoggerWrapper.swift
@@ -1,5 +1,4 @@
 import Foundation
-import SwiftyBeaver
 
 /// Logger with specified label
 public final class LoggerWrapper : NSObject {
@@ -15,25 +14,22 @@ public final class LoggerWrapper : NSObject {
     public func info(_ message: String?, _ file: String = #file, _ function: String = #function, line: Int = #line, customLabel: String? = nil) {
         guard let message = message else { return }
 
-        SwiftyBeaver.info(message, file, function, line: line, context: label)
     }
     @objc
     public func debug(_ message: String?, _ file: String = #file, _ function: String = #function, line: Int = #line, customLabel: String? = nil) {
         guard let message = message else { return }
 
-        SwiftyBeaver.debug(message, file, function, line: line, context: label)
     }
     @objc
     public func warn(_ message: String?, _ file: String = #file, _ function: String = #function, line: Int = #line, customLabel: String? = nil) {
         guard let message = message else { return }
 
-        SwiftyBeaver.warning(message, file, function, line: line, context: label)
+
     }
     @objc
     public func error(_ message: String?, _ file: String = #file, _ function: String = #function, line: Int = #line, customLabel: String? = nil) {
         guard let message = message else { return }
 
-        SwiftyBeaver.error(message, file, function, line: line, context: label)
     }
 
 
@@ -43,24 +39,21 @@ public final class LoggerWrapper : NSObject {
     public static func info(_ label: String, _ message: String?, _ file: String = #file, _ function: String = #function, line: Int = #line) {
         guard let message = message else { return }
 
-        SwiftyBeaver.info(message, file, function, line: line, context: label)
     }
     @objc
     public static func debug(_ label: String, _ message: String?, _ file: String = #file, _ function: String = #function, line: Int = #line) {
         guard let message = message else { return }
 
-        SwiftyBeaver.debug(message, file, function, line: line, context: label)
     }
     @objc
     public static func warn(_ label: String, _ message: String?, _ file: String = #file, _ function: String = #function, line: Int = #line) {
         guard let message = message else { return }
 
-        SwiftyBeaver.warning(message, file, function, line: line, context: label)
+
     }
     @objc
     public static func error(_ label: String, _ message: String?, _ file: String = #file, _ function: String = #function, line: Int = #line) {
         guard let message = message else { return }
 
-        SwiftyBeaver.error(message, file, function, line: line, context: label)
     }
 }
